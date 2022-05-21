adb7494334d1b91e35af35e54880b943f9f80ffe
ttronge <tomastronge@gmail.com>
2022-05-21 16:43:10 -0300

    a

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+
+ReactDOM.render(
+    // <App saludo="Oli" />,
+    <App>
+      Buenasssssssssss
+    </App>,
+  document.getElementById('root')
+);
